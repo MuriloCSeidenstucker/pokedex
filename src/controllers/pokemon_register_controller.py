87fea27d8ff56b686324730d3b1eaad48c62ac1f
Murilo Seidenstucker <murilocampos.004@gmail.com>
2025-06-26 08:59:18 -0300

    test: add unit tests for PokemonRegisterController

--- src/controllers/pokemon_register_controller.py
+++ src/controllers/pokemon_register_controller.py
@@ -4,10 +4,24 @@ from typing import Dict
 class PokemonRegisterController:
     def register(self, new_pokemon_info: Dict) -> Dict:
         try:
+            self.__validate_fields(new_pokemon_info)
             response = self.__format_response(new_pokemon_info)
             return {"success": True, "message": response}
         except Exception as e:
             return {"success": False, "error": str(e)}
+
+    def __validate_fields(self, new_pokemon_info: Dict) -> None:
+        if (
+            not isinstance(new_pokemon_info["name"], str)
+            or not new_pokemon_info["name"]
+        ):
+            raise Exception("Campo nome incorreto!")
+
+        if (
+            not isinstance(new_pokemon_info["type"], str)
+            or not new_pokemon_info["type"]
+        ):
+            raise Exception("Campo tipo incorreto!")
 
     def __format_response(self, new_pokemon_info: Dict) -> Dict:
         return {"count": 1, "type": "Pokemon", "attributes": new_pokemon_info}
--- tests/controllers/pokemon_register_controller_test.py
+++ tests/controllers/pokemon_register_controller_test.py
@@ -6,4 +6,26 @@ def test_register() -> None:
     pokemon_register_controller = PokemonRegisterController()
     response = pokemon_register_controller.register(mock_info)
 
-    print(response)
+    assert response["success"]
+    assert response["message"]["count"] == 1
+    assert response["message"]["type"] == "Pokemon"
+    assert response["message"]["attributes"]["name"] == mock_info["name"]
+    assert response["message"]["attributes"]["type"] == mock_info["type"]
+
+
+def test_register_name_error() -> None:
+    mock_info = {"name": "", "type": "Elétrico"}
+    pokemon_register_controller = PokemonRegisterController()
+    response = pokemon_register_controller.register(mock_info)
+
+    assert not response["success"]
+    assert response["error"] == "Campo nome incorreto!"
+
+
+def test_register_type_error() -> None:
+    mock_info = {"name": "Pikachu", "type": 123}
+    pokemon_register_controller = PokemonRegisterController()
+    response = pokemon_register_controller.register(mock_info)
+
+    assert not response["success"]
+    assert response["error"] == "Campo tipo incorreto!"
