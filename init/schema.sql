ff34e5351e46740fdde487f6bf2059c37e533f3a
Murilo Seidenstucker <murilocampos.004@gmail.com>
2025-07-02 14:40:32 -0300

    build: add initial schema.sql with pokemons table definition

---
+++ init/schema.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE IF NOT EXISTS pokedex;
+
+CREATE TABLE IF NOT EXISTS `pokedex`.`pokemons` (
+    pokemon_id BIGINT NOT NULL,
+    pkn_name VARCHAR(255) NOT NULL,
+    type_1 VARCHAR(255) NOT NULL,
+    type_2 VARCHAR(255),
+    generation BIGINT NOT NULL,
+    is_legendary BIGINT NOT NULL,
+    PRIMARY KEY (pokemon_id)
+);
