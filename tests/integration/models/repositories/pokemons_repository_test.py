cda2f64022d4a07b55d3cfb8266437a233d6dc0d
Murilo Seidenstucker <murilocampos.004@gmail.com>
2025-07-03 12:14:09 -0300

    test(repository): add real DB test for select_all_pokemons in PokemonsRepository

--- tests/integration/models/repositories/pokemons_repository_test.py
+++ tests/integration/models/repositories/pokemons_repository_test.py
@@ -1,3 +1,7 @@
+# pylint: disable=W0622:redefined-builtin
+
+from typing import List
+
 import pytest
 from sqlalchemy import text
 
@@ -32,6 +36,37 @@ def test_insert_pokemon_with_real_mysql():
     assert registry.pokemon_id == int(request["pokemon_id"])
 
     __data_reset(registry)
+
+
+@pytest.mark.skip(reason="sensitive test")
+def test_select_all_pokemons_with_real_mysql():
+    repo = PokemonsRepository()
+    ids = []
+    for id in range(5):
+        request = {
+            "pokemon_id": str(id),
+            "pkn_name": "Bulbasaur",
+            "type_1": "Grass",
+            "type_2": "Poison",
+            "generation": "1",
+            "is_legendary": "0",
+        }
+        repo.insert_pokemon(request)
+        ids.append(id)
+
+    response = repo.select_all_pokemons()
+
+    assert isinstance(response, List)
+    assert len(response) == len(ids)
+    for id in ids:
+        assert response[id].pokemon_id == id
+        assert response[id].pkn_name == request["pkn_name"]
+        assert response[id].type_1 == request["type_1"]
+        assert response[id].type_2 == request["type_2"]
+        assert response[id].generation == int(request["generation"])
+        assert response[id].is_legendary == int(request["is_legendary"])
+
+    __data_reset(ids)
 
 
 def __data_reset(ids):
