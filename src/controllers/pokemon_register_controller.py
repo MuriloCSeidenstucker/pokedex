6c880c3614620b4f9e5ebe83da8b40801ef4d934
Murilo Seidenstucker <murilocampos.004@gmail.com>
2025-07-05 18:14:26 -0300

    feat(controller): call repository to persist data in PokemonRegisterController

--- src/controllers/pokemon_register_controller.py
+++ src/controllers/pokemon_register_controller.py
@@ -1,27 +1,46 @@
 from typing import Dict
 
+from src.common.pokemon import Pokemon
+from src.models.repositories.pokemons_repository import PokemonsRepository
+
 
 class PokemonRegisterController:
+    def __init__(self, pokemon_repository: PokemonsRepository) -> None:
+        self.__pokemon_repository = pokemon_repository
+
     def register(self, new_pokemon_info: Dict) -> Dict:
         try:
             self.__validate_fields(new_pokemon_info)
+            self.__insert_pokemon(new_pokemon_info)
             response = self.__format_response(new_pokemon_info)
             return {"success": True, "message": response}
         except Exception as e:
             return {"success": False, "error": str(e)}
 
     def __validate_fields(self, new_pokemon_info: Dict) -> None:
-        if (
-            not isinstance(new_pokemon_info["name"], str)
-            or not new_pokemon_info["name"]
-        ):
-            raise Exception("Campo nome incorreto!")
+        if not isinstance(new_pokemon_info, Dict):
+            raise Exception(
+                f"Invalid argument type: {type(new_pokemon_info)}. Must be a dictionary"
+            )
 
-        if (
-            not isinstance(new_pokemon_info["type"], str)
-            or not new_pokemon_info["type"]
-        ):
-            raise Exception("Campo tipo incorreto!")
+        try:
+            int(new_pokemon_info["pokemon_id"])
+            int(new_pokemon_info["generation"])
+            int(new_pokemon_info["is_legendary"])
+        except Exception as e:
+            raise e
+
+    def __insert_pokemon(self, new_pokemon_info: Dict) -> None:
+        pokemon = Pokemon(
+            pokemon_id=new_pokemon_info["pokemon_id"],
+            pkn_name=new_pokemon_info["pkn_name"],
+            type_1=new_pokemon_info["type_1"],
+            type_2=new_pokemon_info["type_2"],
+            generation=new_pokemon_info["generation"],
+            is_legendary=new_pokemon_info["is_legendary"],
+        )
+
+        self.__pokemon_repository.insert_pokemon(pokemon)
 
     def __format_response(self, new_pokemon_info: Dict) -> Dict:
         return {"count": 1, "type": "Pokemon", "attributes": new_pokemon_info}
--- src/main/constructors/pokemon_register_constructor.py
+++ src/main/constructors/pokemon_register_constructor.py
@@ -1,10 +1,12 @@
 from src.controllers.pokemon_register_controller import PokemonRegisterController
+from src.models.repositories.pokemons_repository import PokemonsRepository
 from src.views.pokemon_register_view import PokemonRegisterView
 
 
 def pokemon_register_constructor():
+    pokemon_repository = PokemonsRepository()
     pokemon_register_view = PokemonRegisterView()
-    pokemon_register_controller = PokemonRegisterController()
+    pokemon_register_controller = PokemonRegisterController(pokemon_repository)
 
     new_pokemon_info = pokemon_register_view.registry_pokemon_view()
     response = pokemon_register_controller.register(new_pokemon_info)
