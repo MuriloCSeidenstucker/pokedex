5b3154213d83ede2cbafff2503a1f449fc5817ba
Murilo Seidenstucker <murilocampos.004@gmail.com>
2025-07-06 10:00:05 -0300

    test(view): add unit tests for PokemonFindAllView

---
+++ tests/unit/views/pokemon_find_all_view_test.py
@@ -0,0 +1,54 @@
+from pytest_mock import MockerFixture
+
+from src.common.pokemon import Pokemon
+from src.views.pokemon_find_all_view import PokemonFindAllView
+
+
+def test_find_all_pokemons_success(mocker: MockerFixture):
+    mock_message = {
+        "attributes": [
+            Pokemon(
+                pokemon_id=1,
+                pkn_name="Bulbasaur",
+                type_1="Grass",
+                type_2="Poison",
+                generation=1,
+                is_legendary=0,
+            ),
+            Pokemon(
+                pokemon_id=2,
+                pkn_name="Ivysaur",
+                type_1="Grass",
+                type_2="Poison",
+                generation=1,
+                is_legendary=0,
+            ),
+        ]
+    }
+
+    mock_add_column = mocker.MagicMock()
+    mocker.patch("rich.table.Table.add_column", side_effect=mock_add_column)
+    mock_add_row = mocker.MagicMock()
+    mocker.patch("rich.table.Table.add_row", side_effect=mock_add_row)
+    mock_print = mocker.MagicMock()
+    mocker.patch("rich.console.Console.print", side_effect=mock_print)
+
+    view = PokemonFindAllView()
+    view.find_all_pokemons_success(mock_message)
+
+    assert mock_add_column.call_count == 5
+    assert mock_add_row.call_count == 2
+    mock_print.assert_called_once()
+
+
+def test_find_all_pokemons_fail(mocker: MockerFixture):
+    mock_system = mocker.MagicMock()
+    mocker.patch("os.system", side_effect=mock_system)
+    mock_print = mocker.MagicMock()
+    mocker.patch("rich.console.Console.print", side_effect=mock_print)
+
+    view = PokemonFindAllView()
+    view.find_all_pokemons_fail("")
+
+    mock_system.assert_called_once()
+    mock_print.assert_called_once()
