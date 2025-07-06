8b320e7f645faf74b99757d26d268ff507ec703a
Murilo Seidenstucker <murilocampos.004@gmail.com>
2025-07-06 19:50:07 -0300

    build(db): set pkn_name column as UNIQUE in pokemons table

--- init/schema.sql
+++ init/schema.sql
@@ -2,7 +2,7 @@ CREATE DATABASE IF NOT EXISTS pokedex;
 
 CREATE TABLE IF NOT EXISTS `pokedex`.`pokemons` (
     pokemon_id BIGINT NOT NULL,
-    pkn_name VARCHAR(255) NOT NULL,
+    pkn_name VARCHAR(255) NOT NULL UNIQUE,
     type_1 VARCHAR(255) NOT NULL,
     type_2 VARCHAR(255),
     generation BIGINT NOT NULL,
