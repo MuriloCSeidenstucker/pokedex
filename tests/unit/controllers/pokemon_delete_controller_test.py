4f41d8af52651de7c1b65a20295a4b96ea347276
Murilo Seidenstucker <murilocampos.004@gmail.com>
2025-07-08 16:43:57 -0300

    test(controller): add unit tests for PokemonDeleteController

---
+++ tests/unit/controllers/pokemon_delete_controller_test.py
@@ -0,0 +1,66 @@
+import pytest
+from pytest_mock import MockerFixture
+
+from src.common.by import By
+from src.common.pokemon import Pokemon
+from src.controllers.pokemon_delete_controller import PokemonDeleteController
+
+
+@pytest.mark.parametrize(
+    "by,value",
+    [
+        ("id", "1"),
+        ("name", "Bulbasaur"),
+    ],
+)
+def test_delete(by: str, value: str, mocker: MockerFixture):
+    expected = Pokemon(
+        pokemon_id=1,
+        pkn_name="Bulbasaur",
+        type_1="Grass",
+        type_2="Poison",
+        generation=1,
+        is_legendary=0,
+    )
+    mock_repo = mocker.patch(
+        "src.models.repositories.pokemons_repository.PokemonsRepository"
+    )
+    mock_repo.delete_pokemon = mocker.MagicMock()
+    mock_repo.delete_pokemon.return_value = expected
+
+    controller = PokemonDeleteController(mock_repo)
+    response = controller.delete(by, value)
+
+    mock_repo.delete_pokemon.assert_called_once_with(by, value)
+    assert response["success"]
+    assert response["message"]["count"] == 1
+    assert response["message"]["type"] == "Pokemon"
+    assert isinstance(response["message"]["attributes"], Pokemon)
+    assert response["message"]["attributes"] == expected
+
+
+@pytest.mark.parametrize(
+    "by,value,expected_error",
+    [
+        (
+            1,
+            "Bulbasaur",
+            f"Argument 'by' must be one of {By.ByType}, got '1'",
+        ),
+        (
+            "name",
+            1,
+            "Invalid type for 'value' argument: expected str, got 'int'",
+        ),
+    ],
+)
+def test_delete_validate_fields_error(by, value, expected_error, mocker: MockerFixture):
+    mock_repo = mocker.patch(
+        "src.models.repositories.pokemons_repository.PokemonsRepository"
+    )
+
+    controller = PokemonDeleteController(mock_repo)
+    response = controller.delete(by, value)
+
+    assert expected_error == response["error"]
+    assert not response["success"]
